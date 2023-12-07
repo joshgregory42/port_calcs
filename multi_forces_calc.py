d32e29d3359b44ba877a610fd68b3448970d9deb
joshgregory42 <josh.gregory@colorado.edu>
2023-12-07 11:02:38 -0700

    Add force_deflect and moment_deflect functions, and plot function for visualizing results

--- multi_forces_calc.py
+++ multi_forces_calc.py
@@ -27,15 +27,15 @@ def force_deflect(I, L, P, E):
     theta_max_deg = (- P * L**2) / (2*E*I) * (180 * np.pi)
 
     df = pd.DataFrame({
-        'Force': forces,
-        'v_max': v_max,
-        'theta_max_deg': theta_max_deg
+        'Force (N)': forces,
+        'Displacement (cm)': v_max*100,
+        'Angular Deflection (deg.)': theta_max_deg
     })
 
     return df
 
 # Calculate the deflection and elastic curve for cantilevered beam due to couple moment M_0 on the end (Hibbeler solids 10th ed., p. 815, fourth diagram from the top)
-def moment_deflect(I, L, E, M_0, x):
+def moment_deflect(I, L, E, M_0):
     """
     Calculate the deflection and elastic curve for a cantilevered beam due to a couple moment M_0 on the end.
 
@@ -56,6 +56,21 @@ def moment_deflect(I, L, E, M_0, x):
 
     return v_max, theta_max
 
+def plot(df, title):
+    plt.plot(df.iloc[:, 0], df.iloc[:, 1], 'r')
+    plt.xlabel(df.columns[0])
+    plt.ylabel(df.columns[1])
+    plt.title(title)
+    plt.show()
+    plt.savefig(title.lower().replace(" ", "_")+'_disp.png', dpi=1000)
+
+    plt.plot(df.iloc[:, 0], df.iloc[:, 2], 'b')
+    plt.xlabel(df.columns[0])
+    plt.ylabel(df.columns[2])
+    plt.title(title)
+    plt.show()
+    plt.savefig(title.lower().replace(" ", "_")+'_ang_disp.png', dpi=1000)
+
 # Young's Moduli
 clear_young = 2080 * 1000000
 durable_young = 994 * 1000000
@@ -72,11 +87,6 @@ I = np.pi*(d_m_outer**4 - d_m_inner**4) / 64 # I_x = I_y = I
 
 L_mm = 145 # For the double balloon
 L = L_mm / 1000
-
-x = np.linspace(start=0, stop=L, num=10000) # Points for elastic curve
-
-decimal_round = 3 # Round to this
-
     
 forces = np.linspace(0, 50, 10000)
 
@@ -89,3 +99,4 @@ durable_biomed.to_csv('durable_biomed.csv', index=False)
 white_biomed = force_deflect(I=I, L=L, P=forces, E=white_young)
 white_biomed.to_csv('white_biomed.csv', index=False)
 
+plot(clear_biomed, title='Clear BioMed Resin')
